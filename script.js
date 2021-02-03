e06db5536c75d89856e62df5018925f861f5b646
André Marques <77750879+Andremarquesgoulart@users.noreply.github.com>
2021-02-03 12:57:30 -0300

    commit conhecimentos

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let hamburguer = document.querySelector('.hamburguer');
+// FUNÇÃO OPEN HAMBURGUER
 
+let hamburguer = document.querySelector('.hamburguer');
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+// FUNÇÃO CLOSE HAMBURGUER
+
+let closeHamburguer = document.querySelector('.hamburguer');
+closeHamburguer.addEventListener("click",function(){
+    document.querySelector(".hamburguer").classList.toggle("show-menu-close");
+})
